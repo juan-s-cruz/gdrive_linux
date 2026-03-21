a6251442edea039c3fafcfdbc8169db76a13e8e5
juan-s-cruz <jscruz19@gmail.com>
2026-03-21 21:17:37 +0100

    advanced version, update README and CHANGELOG

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gdrive_client",
-    version="0.1.0",
+    version="0.2.0",
     description="Minimal Google Drive Linux Client",
     packages=find_packages(),
     py_modules=["main"],
